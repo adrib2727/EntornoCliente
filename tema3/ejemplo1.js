b5fd4a3c19a510e1678f8d32a7d90f002d88d0f1
adrib2727 <1464795@alu.murciaeduca.es>
2020-10-07 18:42:07 +0200

    Segunda subida

---
+++ tema3/ejemplo1.js
@@ -0,0 +1,2 @@
+var radio = parseInt(prompt("Dime el valor del radio")); //Le das a radio el valor de la fución.
+document.write(`El área de una circunferencia con radio ${radio} es ${Math.PI*(Math.pow(radio,2))}`);
